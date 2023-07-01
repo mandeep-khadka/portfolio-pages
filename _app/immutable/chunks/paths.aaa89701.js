f01c04ea8ef54ee0df06cffad8d7a978d797c8ee
mak627 <mkhadka69@gmail.com>
2023-07-01 12:22:07 +0200

    Updates

--- _app/immutable/chunks/paths.aaa89701.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_cfxm5u)==null?void 0:s.base)??"/portfolio-pages";var a;const t=((a=globalThis.__sveltekit_cfxm5u)==null?void 0:a.assets)??e;export{t as a,e as b};
